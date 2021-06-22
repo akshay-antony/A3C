811eddcff76a5fd6d465da46e4a52da66956a72c
akshay-antony <aantony@me.iitr.ac.in>
2021-06-22 15:09:10 +0530

    ready to run

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 #import gym
 import torch
-from torch import multiprocessing as mp
+import  torch.multiprocessing as mp
 import argparse
 from model import Model
-from torch.distributions.categorical import Categorical
+from shared_optimizer import SharedAdam
+from methods import train
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="selecting the gym env")
     parser.add_argument("-env_name",default="CartPole-v0")
     args = parser.parse_args()
-    print(mp.gpu_count())
+    num_processes = mp.cpu_count()
+
+    env_name = "CartPole-v0"
+    env = gym.make(env_name)
+    shared_model = Model(env.observation_space.shape[0], env.action_space.n)
+    shared_model.share_memory()
+    global_ep_no = mp.Value('i', 0)
+    shared_optimizer = SharedAdam(shared_model.parameters())
+    processes = []
+
+    max_no_steps = 100
+    max_no_episodes = 50
+    for i in range(num_processes):
+        p = mp.Process(target= train, args=(env_name, shared_model, max_no_steps, max_no_episodes, shared_optimizer, i, global_ep_no))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
 
 
-    #
+
     # li = []
     # for i in range(10):
     #     li.append(torch.rand(4))
--- methods.py
+++ methods.py
@@ -1,7 +1,7 @@
 import torch
 import gym
 from model import Model
-
+import torch.multiprocessing as mp
 
 def calculate_target(final_v, rewards, gamma):
     target = []
@@ -12,27 +12,48 @@ def calculate_target(final_v, rewards, gamma):
     return torch.as_tensor(target)
 
 
-def train(env_name, shared_model, max_no_steps, max_no_episodes, shared_optimizer):
+def train(env_name, shared_model, max_no_steps, max_no_episodes, shared_optimizer, process_id, global_ep_no):
     env = gym.make(env_name)
     observation_no = env.observation.shape[0]
     action_no = env.action_space.n
     model = Model(observation_no, action_no)
 
-
     for i in range(max_no_episodes):
+        model.load_state_dict(shared_model.state_dict())
         state = env.reset()
-        state = torch.as_tensor(state)
+        time_step = 0
+        #state = torch.as_tensor(state)
         ep_tot_rew = 0
         ep_rew, ep_act, ep_state = [], [], []
         while True:
-            action = model.action(state)
-            #n_state, rew, done _ = env.step(action)
-            ep_tot_rew + = rew
+            time_step = time_step + 1
+            action = model.action(torch.as_tensor(state))
+            n_state, rew, done, _ = env.step(action)
+            ep_tot_rew  = ep_tot_rew + rew
+            ep_rew.append(rew)
+            ep_act.append(action)
+            ep_state.append(state)
+            state = n_state
+            if time_step > max_no_steps or done:
+                if done:
+                    targets = calculate_target(0, ep_rew, gamma=0.9)
+                else:
+                    _, critic_value = model.forward(torch.as_tensor(n_state))
+                    targets = calculate_target(critic_value, ep_rew, gamma=0.9)
+                loss = model.calculate_loss(targets=targets,states=torch.as_tensor(ep_state), actions=torch.as_tensor(ep_act))
+                shared_optimizer.zero_grad()
+                loss.backward()
+                for g_net, l_net in zip(shared_model.parameters(), model.parameters()):
+                    g_net._grad = l_net.grad
+                shared_optimizer.step()
+                with global_ep_no.get_lock():
+                    global_ep_no.value += 1
+                print("Global Episode No: {} Process ID: {} Episode No: {} Total Reward: {}".format(global_ep_no.value,process_id, i, ep_tot_rew))
+                break
 
 
 
-if __name__ == '__main__':
-    final_v = 6
-    rewards = torch.Tensor([1,2,3,1,5])+torch.Tensor([1,2,3,3,3])
-    print(rewards)
-    print(calculate_target(final_v, rewards, 0.9))
+    # final_v = 6
+    # rewards = torch.Tensor([1,2,3,1,5])+torch.Tensor([1,2,3,3,3])
+    # print(rewards)
+    # print(calculate_target(final_v, rewards, 0.9))
